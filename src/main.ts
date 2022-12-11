ef61f6defe44dc839ea9f98baa5c25c463b611d0
Sudono Tanjung <sudono.tanjung@klikdaily.com>
2022-12-11 11:47:17 +0700

    add swagger and kube.yaml

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,21 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { setup } from './setup';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setup(app);
+
+  const config = new DocumentBuilder()
+   .setTitle('Auth Micro Services Documentation ')
+   .setDescription('The API is use for micro services architecture deployment for authentication')
+   .setVersion('1.0')
+   .addTag('auth')
+   .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
